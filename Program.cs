65a92802c7248e30baa77b6586a04f99c66136ed
pranali2808 <ranepranali1997@gmail.com>
2022-04-02 11:21:59 +0530

    calculate employee month wage

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace EmployeeWageNew
             DailyWage.GetDailyWage();
             PartTimeWage.PartTimeEmpWage();
             SwitchCase.DailyEmpWage();
+            WageMonth.MonthlyEmpWage();
             Console.ReadLine();
             
         }
