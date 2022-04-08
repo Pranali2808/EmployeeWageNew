2bdb762090505f013f45a2f8530444131dfd711c
pranali2808 <ranepranali1997@gmail.com>
2022-04-08 17:47:14 +0530

    refactor ti have list of multiole companies to manage empwage

--- Program.cs
+++ Program.cs
@@ -41,6 +41,12 @@ namespace EmployeeWageNew
             //empWageBuilder.AddCompanyEmpWage("Deloitte", 30, 3, 30);
             //empWageBuilder.ComputeEmpWage();
             //UC11
+            //EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            //empWageBuilder.AddCompanyEmpWage("Google", 10, 4, 20);
+            //empWageBuilder.AddCompanyEmpWage("Microsoft", 20, 5, 20);
+            //empWageBuilder.AddCompanyEmpWage("Deloitte", 30, 3, 30);
+            //empWageBuilder.ComputeEmpWage();
+            //UC-12 call methods
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
             empWageBuilder.AddCompanyEmpWage("Google", 10, 4, 20);
             empWageBuilder.AddCompanyEmpWage("Microsoft", 20, 5, 20);
